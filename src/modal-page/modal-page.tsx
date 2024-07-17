58730175dbec88209e59b6f451e681f2a5aea77a
MartinNuc <martin.nuc@productboard.com>
2024-07-17 14:21:59 +0200

    Modal - useImperativeHandle

--- src/modal-page/modal-page.tsx
+++ src/modal-page/modal-page.tsx
@@ -1,14 +1,18 @@
-import { Modal } from "./modal"
+import { useRef } from "react";
+import { Modal, ModalApi } from "./modal"
 
 export const ModalPage = () => {
 
+  const modalRef = useRef<ModalApi | null>(null);
+
   return <div>
+    <button onClick={() => modalRef.current?.open()}>Open modal</button>
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis neque tempora voluptatem autem. Esse eum maxime officiis minima expedita laboriosam molestiae at voluptas error deserunt aliquid amet, dignissimos quae sint. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, consectetur nihil veniam voluptatibus rerum minus molestiae in numquam voluptas assumenda, perspiciatis voluptatum ratione similique obcaecati nisi. Explicabo animi consequatur architecto!
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis neque tempora voluptatem autem. Esse eum maxime officiis minima expedita laboriosam molestiae at voluptas error deserunt aliquid amet, dignissimos quae sint. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, consectetur nihil veniam voluptatibus rerum minus molestiae in numquam voluptas assumenda, perspiciatis voluptatum ratione similique obcaecati nisi. Explicabo animi consequatur architecto!
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis neque tempora voluptatem autem. Esse eum maxime officiis minima expedita laboriosam molestiae at voluptas error deserunt aliquid amet, dignissimos quae sint. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, consectetur nihil veniam voluptatibus rerum minus molestiae in numquam voluptas assumenda, perspiciatis voluptatum ratione similique obcaecati nisi. Explicabo animi consequatur architecto!
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis neque tempora voluptatem autem. Esse eum maxime officiis minima expedita laboriosam molestiae at voluptas error deserunt aliquid amet, dignissimos quae sint. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, consectetur nihil veniam voluptatibus rerum minus molestiae in numquam voluptas assumenda, perspiciatis voluptatum ratione similique obcaecati nisi. Explicabo animi consequatur architecto!
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis neque tempora voluptatem autem. Esse eum maxime officiis minima expedita laboriosam molestiae at voluptas error deserunt aliquid amet, dignissimos quae sint. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, consectetur nihil veniam voluptatibus rerum minus molestiae in numquam voluptas assumenda, perspiciatis voluptatum ratione similique obcaecati nisi. Explicabo animi consequatur architecto!
-    <Modal>
+    <Modal ref={modalRef}>
       <h1>Hello</h1>
       <p>I am modal</p>
     </Modal>
--- src/modal-page/modal.tsx
+++ src/modal-page/modal.tsx
@@ -1,7 +1,21 @@
-import { PropsWithChildren } from "react";
+import { forwardRef, PropsWithChildren, useImperativeHandle, useState } from "react";
 import { createPortal } from "react-dom";
 
-export function Modal({ children }: PropsWithChildren) {
+export type ModalApi = {
+  open: () => void;
+  close: () => void;
+}
+
+export const Modal = forwardRef<ModalApi, PropsWithChildren>(function Modal({ children }: PropsWithChildren, ref) {
+
+  const [isVisible, setVisible] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    open: () => setVisible(true),
+    close: () => setVisible(false)
+  }), []);
+
+  if (!isVisible) { return null; }
 
   return createPortal((
       <div style={{
@@ -12,7 +26,8 @@ export function Modal({ children }: PropsWithChildren) {
          border: '1px solid black',
          background: 'white' 
       }}>
+        <button onClick={() => setVisible(false)}>X</button>
         {children}
       </div>
     ), document.body);
-  }
+  });
